7bb58afbb3fab9248242cd2750ad522bba305f79
HexedRevii <143884194+hexedrevii@users.noreply.github.com>
2025-09-18 00:33:37 +0300

    Add worldController.

---
+++ worldController.lua
@@ -0,0 +1,46 @@
+--[[
+  Copyright 2025 HexedRevii
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+]]
+
+---@class worldController
+---@field active world?
+local worldController = {}
+worldController.__index = worldController
+
+---@class world
+---@field init function?
+---@field update function
+---@field draw function
+local _world = {}
+
+function worldController:new()
+  local w = {
+    active = nil
+  }
+
+  return setmetatable(w, worldController)
+end
+
+---@param new world
+function worldController:set(new)
+  self.active = new
+  if self.active.init then
+    self.active:init()
+  end
+end
+
+function worldController:update(delta)
+  self.active:update(delta)
+end
+
+function worldController:draw()
+  self.active:draw()
+end
+
+return worldController
